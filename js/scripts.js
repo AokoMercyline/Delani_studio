0adcaf8f66cdebe14fb2ac9f013494dbd7d94388
AokoMercyline <aokomercyline34@gmail.com>
2020-07-26 17:50:52 +0300

    modified index.html and added content on js and styled

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".clickable").click(function(){
+        $("#para-1").toggle();
+        // $("#para-1").click(function(){
+            $("#image-1").toggle();
+        // });
+    
+    });
+    
+});
+
