8962b77bcd81aeafa7379fd022c46da268c368f7
AokoMercyline <aokomercyline34@gmail.com>
2020-07-27 13:25:17 +0300

    deleted added and styled body

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
 $(document).ready(function(){
-    $(".clickable").click(function(){
-        $("#para-1").toggle();
-        // $("#para-1").click(function(){
-            $("#image-1").toggle();
-        // });
-    
+    $(".image1").click(function(){
+        $(".image1").slideDown("1500").hide(1000);
+            $("#para1").show("1500");
+    });
+    $("#para1").click(function(){
+        $("#para1").slideUp("1500").hide(1000);
+            $(".image1").slideDown("1500");
+    });
     });
     
+
+$(document).ready(function(){
+    $(".image2").click(function(){
+        $(".image2").slideDown("1500").hide(1000);
+        $("#para2").show("1500");
+    });
+    $("#para2").click(function(){
+        $("#para2").slideUp("1500").hide(1000);
+        $(".image2").show("1500");
+    });
 });
 
 $(document).ready(function(){
-    $(".clickable1").click(function(){
-        $("#para-2").toggle();
-        $("#image-2").toggle();
+    $(".image3").click(function(){
+        $(".image3").slideDown("1500").hide(1000);
+        $("#para3").show("1500");
     });
-});
-
-$(document).ready(function(){
-    $(".clickable2").click(function(){
-        $("#para-3").toggle();
-        $("#image-3").toggle();
+    $("#para3").click(function(){
+        $("#para3").slideUp("1500").hide(1000);
+        $(".image3").show("1500");
     });
+    
 });
 
 $(document).ready(function(){
@@ -36,36 +46,37 @@ $(document).ready(function(){
         });
         $(".text").hide();
     });
-
-// sample of submit
-function Mercyy(){
-    let username=document.getElementById("name").value;
-    let email=document.getElementById("email").value;
-    alert(username + " THANK YOU FOR CONTACTING US.WE HAVE GLADLY RECIEVED YOUR MESSAGE ");
-}
-
-
-
-// sample jQuery
-$(document).ready(function(){
-    $(".Amwata").hover(function(){
-        $(".Mercy").show();
-        $(".Amwata img").css({
-            opacity:0.4
-        });
-    });
-    $(".Amwata").mouseleave(function(){
-        $(".Amwata img").css({
-            opacity:1
-        });
-        $(".Mercy").hide();
-    });
-
-    // SAMPLE WHAT WE DO
-
-    $(".design").click(function(){
-        $(".para-1").toggle();
-        $(". img").toggle();
-    });
 });
+
+// // sample of submit
+// function Mercyy(){
+//     let username=document.getElementById("name").value;
+//     let email=document.getElementById("email").value;
+//     alert(username + " THANK YOU FOR CONTACTING US.WE HAVE GLADLY RECIEVED YOUR MESSAGE ");
+// }
+
+
+
+// // sample jQuery
+// $(document).ready(function(){
+//     $(".Ann").hover(function(){
+//         $(".Mercy").show();
+//         $(".Ann img").css({
+//             opacity:0.4
+//         });
+//     });
+//     $(".Ann").mouseleave(function(){
+//         $(".Ann img").css({
+//             opacity:1
+//         });
+//         $(".Mercy").hide();
+//     });
+
+//     // SAMPLE WHAT WE DO
+
+// //     $(".design").click(function(){
+// //         $(".para-1").toggle();
+// //         $(". img").toggle();
+// //     });
+// // });
 
