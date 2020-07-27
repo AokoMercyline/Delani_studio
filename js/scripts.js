6233d777b5224da9454fa55026919143ecc22ce1
AokoMercyline <aokomercyline34@gmail.com>
2020-07-27 15:15:57 +0300

    modified index.html scripts and styled

--- js/scripts.js
+++ js/scripts.js
@@ -34,18 +34,115 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $(".image").hover(function(){
-        $(".text").show();
-        $(".image img").css({
+    $(".studio").hover(function(){
+        $(".text1").show();
+        $(".studio img").css({
+            opacity:0.6
+        });
+    });
+    $(".studio").mouseleave(function(){
+        $(".studio img").css({
+            opacity:0.4
+        });
+        $(".text1").hide();
+    });
+    $(".ontario").hover(function(){
+        $(".text2").show();
+        $(".ontario img").css({
             opacity:0.4
         });
     });
-    $(".image").mouseleave(function(){
-        $(".image img").css({
+    $(".ontario").mouseleave(function(){
+        $(".ontario img").css({
             opacity:1
         });
-        $(".text").hide();
+        $(".text2").hide();
     });
+    $(".pyramid").hover(function(){
+        $(".text3").show();
+        $(".pyramid img").css({
+            opacity:0.4
+        });
+    });
+    $(".pyramid").mouseleave(function(){
+        $(".pyramid img").css({
+            opacity:1
+        });
+        $(".text3").hide();
+    });
+    $(".studio3").hover(function(){
+        $(".text3").show();
+        $(".studio3 img").css({
+            opacity:0.4
+        });
+    });
+    $(".studio3").mouseleave(function(){
+        $(".studio3 img").css({
+            opacity:1
+        });
+        $(".text3").hide();
+    });
+    $(".studio4").hover(function(){
+        $(".text4").show();
+        $(".studio4 img").css({
+            opacity:0.4
+        });
+    });
+    $(".studio4").mouseleave(function(){
+        $(".studi04 img").css({
+            opacity:1
+        });
+        $(".text4").hide();
+    });
+    $(".studio5").hover(function(){
+        $(".text5").show();
+        $(".studio5 img").css({
+            opacity:0.4
+        });
+    });
+    $(".studio5").mouseleave(function(){
+        $(".studi5o img").css({
+            opacity:1
+        });
+        $(".text5").hide();
+    });
+    $(".studio6").hover(function(){
+        $(".text6").show();
+        $(".studio6 img").css({
+            opacity:0.4
+        });
+    });
+    $(".studio6").mouseleave(function(){
+        $(".studio6 img").css({
+            opacity:1
+        });
+        $(".text6").hide();
+    });
+    $(".studio7").hover(function(){
+        $(".text7").show();
+        $(".studio7 img").css({
+            opacity:0.4
+        });
+    });
+    $(".studio7").mouseleave(function(){
+        $(".studio7 img").css({
+            opacity:1
+        });
+        $(".text7").hide();
+    });
+    $(".studio8").hover(function(){
+        $(".text8").show();
+        $(".studio8 img").css({
+            opacity:0.4
+        });
+    });
+    $(".studio8").mouseleave(function(){
+        $(".studio8 img").css({
+            opacity:1
+        });
+        $(".text8").hide();
+    });
+
 });
 
 // // sample of submit
