28524facd5bfc7fd68454bc746b289360ada3907
AokoMercyline <aokomercyline34@gmail.com>
2020-07-28 09:32:02 +0300

    modified body

--- js/scripts.js
+++ js/scripts.js
@@ -1,179 +1,157 @@
 $(document).ready(function(){
     $(".image1").click(function(){
-        $(".image1").slideDown("1500").hide(1000);
-            $("#para1").show("1500");
+        $(".image1").slideDown(1500).hide(1000);
+            $("#para1").show(1500);
     });
     $("#para1").click(function(){
-        $("#para1").slideUp("1500").hide(1000);
-            $(".image1").slideDown("1500");
+        $("#para1").slideUp(1500).hide(1000);
+            $(".image1").slideDown(1500);
     });
     });
     
 
 $(document).ready(function(){
     $(".image2").click(function(){
-        $(".image2").slideDown("1500").hide(1000);
-        $("#para2").show("1500");
+        $(".image2").slideDown(1500).hide(1000);
+        $("#para2").show(1500);
     });
     $("#para2").click(function(){
-        $("#para2").slideUp("1500").hide(1000);
-        $(".image2").show("1500");
+        $("#para2").slideUp(1500).hide(1000);
+        $(".image2").show(1500);
     });
 });
 
 $(document).ready(function(){
     $(".image3").click(function(){
-        $(".image3").slideDown("1500").hide(1000);
-        $("#para3").show("1500");
+        $(".image3").slideDown(1500).hide(1000);
+        $("#para3").show(1500);
     });
     $("#para3").click(function(){
-        $("#para3").slideUp("1500").hide(1000);
-        $(".image3").show("1500");
+        $("#para3").slideUp(1500).hide(1000);
+        $(".image3").show(1500);
     });
     
 });
 
 $(document).ready(function(){
     $(".studio").hover(function(){
-        $(".text1").show();
+        $(".overlay").show();
         $(".studio img").css({
             opacity:0.6
         });
     });
     $(".studio").mouseleave(function(){
         $(".studio img").css({
-            opacity:0.4
+            opacity:0.6
         });
-        $(".text1").hide();
+        $(".overlay").hide();
     });
     $(".ontario").hover(function(){
-        $(".text2").show();
+        $(".overlay").show();
         $(".ontario img").css({
-            opacity:0.4
+            opacity:0.6
         });
     });
     $(".ontario").mouseleave(function(){
         $(".ontario img").css({
-            opacity:1
+            opacity:0.6
         });
-        $(".text2").hide();
+        $(".overlay").hide();
     });
     $(".pyramid").hover(function(){
-        $(".text3").show();
+        $(".overlay").show();
         $(".pyramid img").css({
-            opacity:0.4
+            opacity:0.6
         });
     });
     $(".pyramid").mouseleave(function(){
         $(".pyramid img").css({
-            opacity:1
+            opacity:0.6
         });
-        $(".text3").hide();
+        $(".overlay").hide();
     });
     $(".studio3").hover(function(){
-        $(".text3").show();
+        $(".overlay").show();
         $(".studio3 img").css({
-            opacity:0.4
+            opacity:0.6
         });
     });
     $(".studio3").mouseleave(function(){
         $(".studio3 img").css({
-            opacity:1
+            opacity:0.6
         });
-        $(".text3").hide();
+        $(".overlay").hide();
     });
     $(".studio4").hover(function(){
-        $(".text4").show();
+        $(".overlay").show();
         $(".studio4 img").css({
-            opacity:0.4
+            opacity:0.6
         });
     });
     $(".studio4").mouseleave(function(){
         $(".studi04 img").css({
-            opacity:1
+            opacity:0.6
         });
-        $(".text4").hide();
+        $(".overlay").hide();
     });
     $(".studio5").hover(function(){
         $(".text5").show();
         $(".studio5 img").css({
-            opacity:0.4
+            opacity:0.6
         });
     });
     $(".studio5").mouseleave(function(){
         $(".studi5o img").css({
-            opacity:1
+            opacity:0.6
         });
-        $(".text5").hide();
+        $(".overlay").hide();
     });
     $(".studio6").hover(function(){
-        $(".text6").show();
+        $(".overlay").show();
         $(".studio6 img").css({
-            opacity:0.4
+            opacity:0.6
         });
     });
     $(".studio6").mouseleave(function(){
         $(".studio6 img").css({
-            opacity:1
+            opacity:0.6
         });
-        $(".text6").hide();
+        $(".overlay").hide();
     });
     $(".studio7").hover(function(){
-        $(".text7").show();
+        $(".overlay").show();
         $(".studio7 img").css({
-            opacity:0.4
+            opacity:0.6
         });
     });
     $(".studio7").mouseleave(function(){
         $(".studio7 img").css({
-            opacity:1
+            opacity:0.6
         });
-        $(".text7").hide();
+        $(".overlay").hide();
     });
     $(".studio8").hover(function(){
-        $(".text8").show();
+        $(".overlay").show();
         $(".studio8 img").css({
-            opacity:0.4
+            opacity:0.6
         });
     });
     $(".studio8").mouseleave(function(){
         $(".studio8 img").css({
-            opacity:1
+            opacity:0.6
         });
-        $(".text8").hide();
+        $(".overlay").hide();
     });
 
 });
 
-// // sample of submit
-// function Mercyy(){
-//     let username=document.getElementById("name").value;
-//     let email=document.getElementById("email").value;
-//     alert(username + " THANK YOU FOR CONTACTING US.WE HAVE GLADLY RECIEVED YOUR MESSAGE ");
-// }
 
+   $(document).ready(function(){
+       $("#form").submit(function(){
+        let username=$("#name").val();
+        let email=$("#email").val();
+        alert(username + " THANK YOU FOR CONTACTING US.WE HAVE GLADLY RECIEVED YOUR MESSAGE ");
+       });
+   });
 
-
-// // sample jQuery
-// $(document).ready(function(){
-//     $(".Ann").hover(function(){
-//         $(".Mercy").show();
-//         $(".Ann img").css({
-//             opacity:0.4
-//         });
-//     });
-//     $(".Ann").mouseleave(function(){
-//         $(".Ann img").css({
-//             opacity:1
-//         });
-//         $(".Mercy").hide();
-//     });
-
-//     // SAMPLE WHAT WE DO
-
-// //     $(".design").click(function(){
-// //         $(".para-1").toggle();
-// //         $(". img").toggle();
-// //     });
-// // });
 
